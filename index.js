0cd995a6cdf28a774176a1048d2337748de18400
tboita <tonyboita@hotmail.com>
2021-07-01 14:40:47 -0300

    adicionando

---
+++ index.js
@@ -0,0 +1,30 @@
+// somar
+
+const divisao = require("./divisao");
+
+const sistema = 'calculadora';
+function somar(a,b){
+    return a + b;
+ }
+console.log(somar(1,2));
+
+function subtrair(a,b){
+    return a - b;
+}
+console.log(subtrair(2,2))
+
+function multiplicar(a,b){
+    return a * b;
+}
+console.log(multiplicar (2,25))
+
+function dividir(a,b){
+    return a / b
+    }
+console.log(dividir(4,2));
+console.log(divisao(10,2));
+
+
+
+
+
